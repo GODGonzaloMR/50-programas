cd3f03f25f313f4c407c2c355cd5cc03b7f96fe8
Gonzalo Cortez Huerta <118482396+GODGonzaloMR@users.noreply.github.com>
2024-11-06 22:20:02 -0800

    Update celsius_a_fahrenheit.s

--- celsius_a_fahrenheit.s
+++ celsius_a_fahrenheit.s
@@ -1,3 +1,29 @@
+# Convertir temperatura de Celsius a Fahrenheit
+```assembly
+/*=======================================================
+ * Programa:     temperatura.s
+ * Autor:        Cortez Huerta  Gonzalo
+ * Fecha:        06 de noviembre de 2024
+ * Descripción:  Convierte una temperatura de Celsius a Fahrenheit usando 
+ *               ensamblador ARM64 en RaspbianOS.
+ * Compilación:  as -o celsius_a_fahrenheit.o celsius_a_fahrenheit.s
+ *               gcc -o celsius_a_fahrenheit celsius_a_fahrenheit.o
+ * Ejecución:    ./celsius_a_fahrenheit.s
+ * Versión:      1.0
+ *
+ * Código equivalente en c:
+ * -----------------------------------------------------
+ * #include <stdio.h> 
+ * int main() { 
+ *     float celsius, fahrenheit; 
+ *     printf("Ingresa la temperatura en Celsius: "); 
+ *     scanf("%f", &celsius); 
+ *     fahrenheit = (celsius * 9 / 5) + 32; 
+ *     printf("Temperatura en Fahrenheit: %.2f\n", fahrenheit); 
+ *     return 0; 
+ * } 
+ * -----------------------------------------------------
+ =========================================================*/
 .section .data
 .section .text
 .global celsius_a_fahrenheit
@@ -15,3 +41,4 @@ celsius_a_fahrenheit:
     add x0, x0, #32         // ((Celsius * 9) / 5) + 32
 
     ret
+ ```
