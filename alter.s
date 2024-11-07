322670dbbff4b6a98deae03a63cbed348cf4a355
Gonzalo Cortez Huerta <118482396+GODGonzaloMR@users.noreply.github.com>
2024-11-06 23:57:04 -0800

    Update alter.s

--- alter.s
+++ alter.s
@@ -1,3 +1,43 @@
+// Autor: Cortez Huerta Gonzalo
+// Fecha: 05/11/2024
+// Descripción: Programa en ensamblador que establece, borra y alterna bits
+
+// Equivalente en C#:
+/*
+using System;
+using System.Runtime.InteropServices;
+
+class Alter
+{
+    // Declaración de las funciones en ensamblador
+    [DllImport("libcalculations.so", CallingConvention = CallingConvention.Cdecl)]
+    public static extern long establecer_bit(long valor, long n);
+    
+    [DllImport("libcalculations.so", CallingConvention = CallingConvention.Cdecl)]
+    public static extern long borrar_bit(long valor, long n);
+    
+    [DllImport("libcalculations.so", CallingConvention = CallingConvention.Cdecl)]
+    public static extern long alternar_bit(long valor, long n);
+
+    static void Main()
+    {
+        Console.WriteLine("Ingrese un número entero:");
+        long valor = Convert.ToInt64(Console.ReadLine());
+        
+        Console.WriteLine("Ingrese la posición del bit (0, 1, 2,...):");
+        long n = Convert.ToInt64(Console.ReadLine());
+
+        long resultadoEstablecer = establecer_bit(valor, n);
+        Console.WriteLine($"Número después de establecer el bit en la posición {n}: {resultadoEstablecer}");
+        
+        long resultadoBorrar = borrar_bit(valor, n);
+        Console.WriteLine($"Número después de borrar el bit en la posición {n}: {resultadoBorrar}");
+        
+        long resultadoAlternar = alternar_bit(valor, n);
+        Console.WriteLine($"Número después de alternar el bit en la posición {n}: {resultadoAlternar}");
+    }
+}
+*/
 .global establecer_bit
 .global borrar_bit
 .global alternar_bit
