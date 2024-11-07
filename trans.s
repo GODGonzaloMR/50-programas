94a7ae3082e61545565b7d009b473de064e7c0f7
Gonzalo Cortez Huerta <118482396+GODGonzaloMR@users.noreply.github.com>
2024-11-06 23:37:22 -0800

    Update trans.s

--- trans.s
+++ trans.s
@@ -1,3 +1,53 @@
+// Autor: Cortez Huerta Gonzalo
+// Fecha: 05/11/2024
+// Descripción: Programa en ensamblador que hace una Transposición de una matriz
+
+// Equivalente en C#:
+/*
+using System;
+using System.Runtime.InteropServices;
+
+class Transposicion
+{
+    // Importar la función de ensamblador
+    [DllImport("libcalculations.so")]
+    public static extern void transponer_matriz(int[] matrizEntrada, int[] matrizSalida, long filas, long columnas);
+
+    static void Main()
+    {
+        Console.Write("Ingrese el número de filas: ");
+        int filas = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el número de columnas: ");
+        int columnas = int.Parse(Console.ReadLine());
+
+        int[] matrizEntrada = new int[filas * columnas];
+        int[] matrizSalida = new int[columnas * filas];
+
+        Console.WriteLine("Ingrese los elementos de la matriz:");
+        for (int i = 0; i < filas; i++)
+        {
+            for (int j = 0; j < columnas; j++)
+            {
+                Console.Write($"Elemento [{i},{j}]: ");
+                matrizEntrada[i * columnas + j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        // Llamada a la función de ensamblador
+        transponer_matriz(matrizEntrada, matrizSalida, filas, columnas);
+
+        Console.WriteLine("Matriz transpuesta:");
+        for (int i = 0; i < columnas; i++)
+        {
+            for (int j = 0; j < filas; j++)
+            {
+                Console.Write(matrizSalida[i * filas + j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+*/
 // trans.s - Transposición de una matriz
 // Entrada: Puntero a la matriz de entrada, puntero a la matriz de salida, filas y columnas de la matriz
 
