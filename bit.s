20ec8534e1ea2e24b5eb6a3ef3ce75e05ed76e94
Gonzalo Cortez Huerta <118482396+GODGonzaloMR@users.noreply.github.com>
2024-11-06 23:58:27 -0800

    Update bit.s

--- bit.s
+++ bit.s
@@ -1,3 +1,28 @@
+// Autor: Cortez Huerta Gonzalo
+// Fecha: 05/11/2024
+// Descripción: Programa en ensamblador que cuenta los bits activados en un número
+
+// Equivalente en C#:
+/*
+using System;
+using System.Runtime.InteropServices;
+
+class BitCounter
+{
+    // Importar la función en ensamblador
+    [DllImport("libcalculations.so", CallingConvention = CallingConvention.Cdecl)]
+    public static extern long contar_bits(long number);
+
+    static void Main()
+    {
+        Console.Write("Ingrese un número: ");
+        long number = Convert.ToInt64(Console.ReadLine());
+
+        long result = contar_bits(number);
+        Console.WriteLine($"El número de bits activados en {number} es: {result}");
+    }
+}
+*/
 .section .text
 .global contar_bits
 
