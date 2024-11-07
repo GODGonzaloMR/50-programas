b68da739588d13932c644c7ad0ddfa86e99ce6ca
Gonzalo Cortez Huerta <118482396+GODGonzaloMR@users.noreply.github.com>
2024-11-06 23:59:35 -0800

    Update mcd.s

--- mcd.s
+++ mcd.s
@@ -1,3 +1,31 @@
+// Autor: Cortez Huerta Gonzalo
+// Fecha: 05/11/2024
+// Descripción: Programa en ensamblador que calcula el Máximo Común Divisor (MCD)
+
+// Equivalente en C#:
+/*
+using System;
+using System.Runtime.InteropServices;
+
+class MCDCalculator
+{
+    // Importar la función en ensamblador
+    [DllImport("libcalculations.so", CallingConvention = CallingConvention.Cdecl)]
+    public static extern long calcular_mcd(long a, long b);
+
+    static void Main()
+    {
+        Console.Write("Ingrese el primer número: ");
+        long a = Convert.ToInt64(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        long b = Convert.ToInt64(Console.ReadLine());
+
+        long result = calcular_mcd(a, b);
+        Console.WriteLine($"El MCD de {a} y {b} es: {result}");
+    }
+}
+*/
 .section .text
 .global calcular_mcd
 
